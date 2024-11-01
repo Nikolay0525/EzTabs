406d96fa224ca40a0e31f313f49cb75e903fc9b2
Nik0_olay <annaksota361@gmail.com>
2024-11-01 10:23:15 +0200

    UI improve

---
+++ EzTabs.View/Window/MainControls/SimpleControls/SliderWithButtons.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EzTabs.View.Window.MainControls.SimpleControls
+{
+    /// <summary>
+    /// Interaction logic for SliderWithButtons.xaml
+    /// </summary>
+    public partial class SliderWithButtons : UserControl
+    {
+        public SliderWithButtons()
+        {
+            InitializeComponent();
+        }
+
+        // Dependency Properties for binding
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register("Minimum", typeof(double), typeof(SliderWithButtons), new PropertyMetadata(0.0));
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum", typeof(double), typeof(SliderWithButtons), new PropertyMetadata(100.0));
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(SliderWithButtons), new PropertyMetadata(50.0));
+
+        public double Minimum
+        {
+            get => (double)GetValue(MinimumProperty);
+            set => SetValue(MinimumProperty, value);
+        }
+
+        public double Maximum
+        {
+            get => (double)GetValue(MaximumProperty);
+            set => SetValue(MaximumProperty, value);
+        }
+
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        // Event handlers for the buttons
+        private void OnMinusButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (Value > Minimum)
+                Value -= 1; // adjust step size here if needed
+        }
+
+        private void OnPlusButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (Value < Maximum)
+                Value += 1; // adjust step size here if needed
+        }
+    }
+}

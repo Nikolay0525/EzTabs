59f85a987b567837239efb6494ac7845d146dbc5
Nikolay0525 <66847324+Nikolay0525@users.noreply.github.com>
2024-11-14 12:42:27 +0200

    Delete EzTabs.Services directory
    Moved to Presentation project

--- EzTabs.Services/NavigationServices/INavigationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EzTabs.Services.NavigationServices
-{
-    public interface INavigationService
-    {
-        void NavigateTo(object viewModel);
-        object CurrentViewModel { get; set; }
-        event Action? CurrentViewModelChanged;
-    }
-}

2f59bb9f392ee50c9eb1e2e1077fe38229d765e4
Nikolay0525 <66847324+Nikolay0525@users.noreply.github.com>
2024-11-14 12:43:02 +0200

    Delete EzTabs.ViewModel directory
    Moved to presentation project

--- EzTabs.ViewModel/ViewModelHelper/IViewModelFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using EzTabs.ViewModel.BaseViewModels;
-
-namespace EzTabs.ViewModel.ViewModelHelper
-{
-    public interface IViewModelFactory
-    {
-        T CreateViewModel<T, U>(U? option) where T : BaseViewModel where U : class;
-    }
-}
